6c6e55370dad14f4fe73af6c312c54fda6296daa
YuJuquan <63937274+YuJuquan@users.noreply.github.com>
2020-05-29 09:42:20 +0800

    Update Trojan_vpn.sh

--- Trojan_vpn.sh
+++ Trojan_vpn.sh
@@ -1,4 +1,5 @@
 # Trojan_vpn.sh
+
 # *************************************
 # 创建者：于钜权                       
 # 创建时间：2020/05/21                            
@@ -20,6 +21,7 @@ red(){
 yellow(){
     echo -e "\033[33m\033[01m$1\033[0m"
 }
+
 
 # Step1:安装Trojan
 function install_trojan(){
@@ -50,8 +52,10 @@ function install_trojan(){
     blue "******************************************************"
     yellow "请输入你的域名："
     blue "******************************************************"
+    
     # 读入域名
     read domain
+    
     # 提取域名
     real_addr=`ping ${domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'` 
     '''
@@ -59,6 +63,7 @@ function install_trojan(){
     # sed命令，用于获得地址的正则表达式
     '''
     local_addr=`curl ipv4.icanhazip.com`
+    
     # 验证域名是否正确
     if [ $real_addr == $local_addr ] ; then
 	    blue "******************************************************"
@@ -71,28 +76,30 @@ function install_trojan(){
         '''
         # 参考：https://www.runoob.com/linux/nginx-install-setup.html
         '''
-	    rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+	rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
         yum install -y nginx
-	    systemctl enable nginx.service
-	    # 设置伪装站
-	    rm -rf /usr/share/nginx/html/*
-	    cd /usr/share/nginx/html/
-	    wget https://github.com/atrandys/v2ray-ws-tls/raw/master/web.zip
+	systemctl enable nginx.service
+	    
+	# 设置伪装站
+	rm -rf /usr/share/nginx/html/*
+	cd /usr/share/nginx/html/
+	wget https://github.com/atrandys/v2ray-ws-tls/raw/master/web.zip
     	unzip web.zip
-	    systemctl restart nginx.service
+	systemctl restart nginx.service
 
-	    # 1.6申请https证书
+	# 1.6申请https证书
         blue "******************************************************"
         green "开始申请https证书"
-	    blue "******************************************************"
+	blue "******************************************************"
         sleep 1s
-	    mkdir /usr/src/trojan-cert
+	mkdir /usr/src/trojan-cert
+	
         # 安装证书
         '''
         # 参考：https://my.oschina.net/u/3042999/blog/1858891
         '''
-	    curl https://get.acme.sh | sh
-	    ~/.acme.sh/
+	curl https://get.acme.sh | sh
+	~/.acme.sh/
         acme.sh  --issue  -d $domain  --webroot /usr/share/nginx/html/
     	~/.acme.sh/acme.sh  --installcert  -d  $domain   \
         --key-file   /usr/src/trojan-cert/private.key \
@@ -100,23 +107,28 @@ function install_trojan(){
         --reloadcmd  "systemctl force-reload  nginx.service"
 
         # 1.7下载Trojan
-	    if test -s /usr/src/trojan-cert/fullchain.cer; then
-            cd /usr/src
+	if test -s /usr/src/trojan-cert/fullchain.cer; then
+        cd /usr/src
+	
         # 下载Trojan服务器
-	    wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.0/trojan-1.14.0-linux-amd64.tar.xz
-	    tar xf trojan-1.*
-	    # 下载Trojan客户端
-	    wget https://github.com/atrandys/trojan/raw/master/trojan-cli.zip
-	    unzip trojan-cli.zip
+	wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.0/trojan-1.14.0-linux-amd64.tar.xz
+	tar xf trojan-1.*
+	
+	# 下载Trojan客户端
+	wget https://github.com/atrandys/trojan/raw/master/trojan-cli.zip
+	unzip trojan-cli.zip
+	
         # 复制证书
         '''
         # 参考：https://www.runoob.com/linux/linux-comm-cp.html
         '''
-	    cp /usr/src/trojan-cert/fullchain.cer /usr/src/trojan-cli/fullchain.cer # cp拷贝证书
+	cp /usr/src/trojan-cert/fullchain.cer /usr/src/trojan-cli/fullchain.cer # cp拷贝证书
+	
         # 写入配置信息
         '''
         # 参考：https://www.runoob.com/linux/linux-comm-cat.html
         '''
+	
         # Trojan客户端配置文件
         cat > /usr/src/trojan-cli/config.json <<-EOF
         {
@@ -151,6 +163,7 @@ function install_trojan(){
             }
         }
 EOF
+
         # Trojan服务器配置文件
         rm -rf /usr/src/trojan/server.conf
         cat > /usr/src/trojan/server.conf <<-EOF
@@ -197,6 +210,7 @@ EOF
         }
 EOF
 
+        # 生成Trojan安装包
         cd /usr/src/trojan-cli/
         zip -q -r trojan-cli.zip /usr/src/trojan-cli/
         trojan_path=$(cat /dev/urandom | head -1 | md5sum | head -c 16) # 16位随机数
@@ -223,31 +237,33 @@ EOF
         [Install]  
         WantedBy=multi-user.target
 EOF
-    
+
+    	# 启动Trojan
 	chmod +x /usr/lib/systemd/system/trojan.service
 	systemctl start trojan.service
 	systemctl enable trojan.service
-    '''
-    https://www.cnblogs.com/zwcry/p/9602756.html
-    '''
+    	'''
+    	https://www.cnblogs.com/zwcry/p/9602756.html
+    	'''
 	blue "******************************************************"
 	green "Trojan已安装完成"
-    green "复制以下链接下载Trojan客户端"
+    	green "复制以下链接下载Trojan客户端"
 	yellow "http://${domain}/$trojan_path/trojan-cli.zip"
 	blue "******************************************************"
 	else
-        red "******************************************************"
+            red "******************************************************"
 	    red "https证书申请失败，请重新申请证书"
-        red "本次安装失败"
+            red "本次安装失败"
 	    red "******************************************************"
 	fi
-    else
-        red "******************************************************"
-        red "域名解析地址与VPS IP地址不一致，请确保域名解析正常"
-        red "本次安装失败"
-        red "******************************************************"
-    fi
+    	else
+            red "******************************************************"
+            red "域名解析地址与VPS IP地址不一致，请确保域名解析正常"
+            red "本次安装失败"
+            red "******************************************************"
+    	fi
 }
+
 
 # Step2:卸载Trojan
 function remove_trojan(){
@@ -266,6 +282,7 @@ function remove_trojan(){
     green "Trojan与nginx卸载完毕"
     blue "******************************************************"
 }
+
 
 # Step3:启动脚本
 start_menu(){
@@ -301,6 +318,7 @@ start_menu(){
         ;;
     esac
 }
+
 
 # 启动脚本
 start_menu
